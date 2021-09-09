3c3ebe775a63eb60584bfcd3826584ce5dcec576
m1172 <muhshuh2000@gmail.com>
2021-09-09 17:43:17 +0200

    edited

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import Navbar from './Navbar';
+import Navbar from './Navbar/index.jsx';
 import Body from './Body';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
         <Body />
+        <Navbar />
       </div>
     );
   }
--- src/Navbar/index.jsx
+++ src/Navbar/index.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import data from '../data';
 
 export default class index extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        {data.map((value) => {
+          return <div>{value.name}</div>;
+        })}
+      </div>
+    );
   }
 }
